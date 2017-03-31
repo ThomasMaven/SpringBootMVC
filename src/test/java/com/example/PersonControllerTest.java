f25a44419a60b7c6da93e0004d6876b3800ccb5d
PGS-SOFT\ttomaka <ttomaka@pgs-software.com>
2017-03-31 15:39:23 +0200

    Add unit and integration tests

---
+++ src/test/java/com/example/PersonControllerTest.java
@@ -0,0 +1,81 @@
+package com.example;
+
+import com.example.Service.PersonService;
+import com.example.model.Person;
+import com.example.repository.PersonRepository;
+import com.example.controller.PersonController;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by ttomaka on 28.03.2017.
+ */
+public class PersonControllerTest {
+
+    @InjectMocks
+    private PersonController personController;
+
+    @Mock
+    private PersonService personService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetPerson() {
+        Person person = new Person();
+        person.setId(1L);
+
+        when(personService.findOne(1L)).thenReturn(person);
+
+        Person per = personController.getPerson(1L);
+
+        verify(personService).findOne(1L);
+
+        assertThat(per, equalTo(person));
+    }
+    @Test
+    public void testGetPerson2() {
+        Person person = new Person();
+        person.setId(1L);
+
+        when(personService.findOne(1L)).thenReturn(person);
+
+        Person per = personController.getPerson(1L);
+
+        verify(personService).findOne(1L);
+
+        assertEquals(per.getId(), person.getId());
+    }
+
+    @Test
+    public void testGetPersonList() {
+        List<Person> personList = new ArrayList<>();
+        for(int i=0;i<3;i++) {
+            Person tmp_person = new Person();
+            tmp_person.setSurname("Surname"+i);
+            tmp_person.setName("Name"+i);
+            personList.add(tmp_person);
+        }
+
+        when(personService.findAll()).thenReturn(personList);
+
+        List<Person> list = personController.personList();
+
+        assertThat(list, equalTo(personList));
+    }
+}
--- src/test/java/com/example/PersonPersistenceTest.java
+++ src/test/java/com/example/PersonPersistenceTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -17,7 +18,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class PersonPersistenceTest {
 
     @Autowired
-    private
     PersonRepository personRepository;
 
     @Test
